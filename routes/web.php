22d24970c73de49203240445c4ba7e1556276fcd
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-11-02 23:40:26 +0200

    Add new GET and POST route to handle customer creation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function() {
     return view('index');
@@ -11,3 +12,8 @@ Route::get('/login', function() {
     return view('login');
 });
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/customers/create', function() {
+    return view('create_customer');
+});
+Route::post('customers/store', [CustomerController::class, 'store'])->name('customer.store');
