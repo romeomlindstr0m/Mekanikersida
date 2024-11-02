fb20e1ce6748eb0cfaa3820be4e2fe086b31552e
lindstrom.romeo <64912856+romeomlindstr0m@users.noreply.github.com>
2024-11-02 23:41:38 +0200

    Functionality: Create customers (#3)
    * Add new CustomerController to store new customers
    
    * Create model to interact with the customers table
    
    * Create StatusBanner component class
    
    * Create customers database table
    
    * Create new view to display customer creation form
    
    * Create StatusBanner component view
    
    * Add new GET and POST route to handle customer creation

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Customer;
+
+class CustomerController extends Controller
+{
+    public function store(Request $request): RedirectResponse
+    {
+        $rules = [
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'phone' => 'required|max:255',
+            'address' => 'required|max:255',
+        ];
+
+        $messages = [
+            'required' => 'Vänligen fyll i alla fält.',
+            'first_name.max' => 'Förnamnet får inte överstiga 255 tecken.',
+            'last_name.max' => 'Efternamnet får inte överstiga 255 tecken.',
+            'phone.max' => 'Telefonnummret får inte överstiga 255 tecken.',
+            'address.max' => 'Adressen får inte överstiga 255 tecken.',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator);
+        }
+
+        $customer = new Customer;
+        
+        $customer->first_name = $request->first_name;
+        $customer->last_name = $request->last_name;
+        $customer->phone = $request->phone;
+        $customer->address = $request->address;
+
+        $customer->save();
+
+        session()->flash('status', 'En ny kund har skapats');
+        return back();
+    }
+}
---
+++ resources/views/components/status-banner.blade.php
@@ -0,0 +1,3 @@
+<div class="w-full bg-gray-800 text-white flex justify-center py-4">    
+    <p class="ms-2 text-sm font-semibold font-inter">{{ $status }}</p>
+</div>
