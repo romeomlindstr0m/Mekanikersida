7e2e4ea871eeddeea31987d6cca44fd3411e5902
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-11-08 01:59:03 +0200

    Dynamically populate the customer combobox with data and correct the form routing

--- resources/views/create_project.blade.php
+++ resources/views/create_project.blade.php
@@ -16,7 +16,7 @@
     @endif
 
     <div class="flex justify-center items-center flex-grow w-3/5">
-      <form class="w-full" action="#" method="#">
+      <form class="w-full" action="{{ route('project.store') }}" method="POST">
         @csrf
         <div class="space-y-12">
           <div class="border-b border-gray-900/10 pb-12">
@@ -85,10 +85,14 @@
                 <div class="mt-2">
                     <div x-data="{
                         allOptions: [
-                            { 
-                                label: 'Acura',
-                                value: 'Acura'
-                            },
+                            @if ($customers)
+                              @foreach ($customers as $customer)
+                                { 
+                                    label: '{{ $customer->first_name . ' ' . $customer->last_name . ' (' . $customer->phone . ')' }}',
+                                    value: '{{ $customer->id }}'
+                                },
+                              @endforeach
+                            @endif
                         ],
                         options: [],
                         isOpen: false,
@@ -102,12 +106,12 @@
                         },
                         getFilteredOptions(query) {
                             this.options = this.allOptions.filter((option) =>
-                                option.label.toLowerCase().includes(query.toLowerCase()),
-                            )
-                            if (this.options.length === 0) {
-                                this.$refs.noResultsMessage.classList.remove('hidden')
+                                option.label.toLowerCase().includes(query.toLowerCase())
+                            );
+                            if (this.options.length === 0 && this.allOptions.length > 0) {
+                                this.$refs.noResultsMessage.classList.remove('hidden');
                             } else {
-                                this.$refs.noResultsMessage.classList.add('hidden')
+                                this.$refs.noResultsMessage.classList.add('hidden');
                             }
                         },
                         handleKeydownOnOptions(event) {
@@ -117,20 +121,27 @@
                         },
                     }" class="flex w-full flex-col gap-1" x-on:keydown="handleKeydownOnOptions($event)" x-on:keydown.esc.window="isOpen = false, openedWithKeyboard = false" x-init="options = allOptions">
                     <div class="relative">
-                        <button type="button" class="inline-flex w-full items-center justify-between gap-2 border border-neutral-300 rounded-md bg-neutral-50 px-4 py-2 text-sm font-medium tracking-wide text-neutral-600 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black" role="combobox" aria-controls="makesList" aria-haspopup="listbox" x-on:click="isOpen = ! isOpen" x-on:keydown.down.prevent="openedWithKeyboard = true" x-on:keydown.enter.prevent="openedWithKeyboard = true" x-on:keydown.space.prevent="openedWithKeyboard = true" x-bind:aria-expanded="isOpen || openedWithKeyboard" x-bind:aria-label="selectedOption ? selectedOption.value : 'Please Select'" >
-                            <span class="text-sm font-normal" x-text="selectedOption ? selectedOption.value : 'Please Select'"></span>
+                        <button type="button" class="inline-flex w-full items-center justify-between gap-2 border border-neutral-300 rounded-md bg-neutral-50 px-4 py-2 text-sm font-medium tracking-wide text-neutral-600 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black" role="combobox" aria-controls="customer_list" aria-haspopup="listbox" x-on:click="isOpen = ! isOpen" x-on:keydown.down.prevent="openedWithKeyboard = true" x-on:keydown.enter.prevent="openedWithKeyboard = true" x-on:keydown.space.prevent="openedWithKeyboard = true" x-bind:aria-expanded="isOpen || openedWithKeyboard" x-bind:aria-label="selectedOption ? selectedOption.value : 'Välj en kund'" >
+                            <span class="text-sm font-normal" x-text="selectedOption ? selectedOption.label : 'Välj en kund'"></span>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"class="size-5" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"/>
                             </svg>
                         </button>
-                        <input id="make" name="make" x-ref="hiddenTextField" hidden=""/>
-                        <div x-show="isOpen || openedWithKeyboard" id="makesList" class="absolute left-0 top-11 z-10 w-full overflow-hidden rounded-md border border-neutral-300 bg-neutral-50" role="listbox" aria-label="industries list" x-on:click.outside="isOpen = false, openedWithKeyboard = false" x-on:keydown.down.prevent="$focus.wrap().next()" x-on:keydown.up.prevent="$focus.wrap().previous()" x-transition x-trap="openedWithKeyboard">
+                        <input id="customer_id" name="customer_id" x-ref="hiddenTextField" hidden=""/>
+                        <div x-show="isOpen || openedWithKeyboard" id="customer_list" class="absolute left-0 top-11 z-10 w-full overflow-hidden rounded-md border border-neutral-300 bg-neutral-50" role="listbox" aria-label="industries list" x-on:click.outside="isOpen = false, openedWithKeyboard = false" x-on:keydown.down.prevent="$focus.wrap().next()" x-on:keydown.up.prevent="$focus.wrap().previous()" x-transition x-trap="openedWithKeyboard">
                             <div class="relative">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="1.5" class="absolute left-4 top-1/2 size-5 -translate-y-1/2 text-neutral-600/50" aria-hidden="true" >
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
                                 </svg>
                                 <input type="text" class="w-full border-b borderneutral-300 bg-neutral-50 py-2.5 pl-11 pr-4 text-sm text-neutral-600 focus:outline-none focus-visible:border-black disabled:cursor-not-allowed disabled:opacity-75" name="searchField" aria-label="Search" x-on:input="getFilteredOptions($el.value)" x-ref="searchField" placeholder="Search" />
                             </div>
+
+                            <template x-if="allOptions.length === 0">
+                              <div class="px-4 py-2 text-sm text-neutral-600">
+                                  Inga kunder hittades
+                              </div>
+                            </template>
+                          
                             <ul class="flex max-h-44 flex-col overflow-y-auto">
                                 <li class="hidden px-4 py-2 text-sm text-neutral-600" x-ref="noResultsMessage">
                                     <span>No matches found</span>
