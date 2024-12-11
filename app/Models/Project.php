0a8113bc863a5693e4b06751cbc555eb901e0961
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-11 15:05:50 +0200

    Define relationships to customers and project statuses models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
-    //
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function project_status(): BelongsTo
+    {
+        return $this->belongsTo(ProjectStatus::class);
+    }
 }
