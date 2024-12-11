90135f93879e1b7cfa3a2492079db5fe16ed27fd
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-11 15:05:28 +0200

    Define relationship to projects model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
-    //
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
